8ae1d4415b96d372c43b5a3cae62197e841abb8c
Danielle <daniellecaricati1@gmail.com>
2026-04-16 16:59:11 -0300

    Inicializando meu projeto

---
+++ src/introducao/HelloWorld.java
@@ -0,0 +1,11 @@
+package introducao;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {  // metodo principal que executa o codigo 
+		// Nossa Logica Aqui
+		System.out.print("Hello World"); // identico ao escreva 
+
+	}
+
+}
