a1f4bf3869668fd12d5d2d3e2a29a3d030fe6480
Danielle <daniellecaricati1@gmail.com>
2026-04-17 16:41:42 -0300

    Adicionando exercicio

---
+++ src/entradaSaida/VariaveisConstantes.java
@@ -0,0 +1,16 @@
+package entradaSaida;
+
+public class VariaveisConstantes {
+
+	public static void main(String[] args) {
+		int idade = 25;
+		String nome = "Lilian";
+		float graus = 20.0f;
+		boolean resposta;
+		
+		final int QUANTIDADE = 50;
+		final String TURMA = "Turma 84";
+		final double AREA = 3.1415;		
+	}
+
+}
