c7db76933591a3bd7c6c8b3b44a262d8fb456ec4
Danielle <daniellecaricati1@gmail.com>
2026-04-22 10:25:21 -0300

    Códigos sobre Estruturas Condicionais

---
+++ src/lacoCondicional/ExemploIF.java
@@ -0,0 +1,23 @@
+package lacoCondicional;
+
+import java.util.Scanner;
+
+public class ExemploIF {
+
+	public static void main(String[] args) {
+		
+		Scanner leia = new Scanner(System.in);
+		int idade;
+		
+		System.out.println("Digite sua idade: ");
+		idade = leia.nextInt();
+		
+		if (idade < 18) {
+			System.out.println("Você não pode dirigir.");
+		}
+		
+		
+
+	}
+
+}
